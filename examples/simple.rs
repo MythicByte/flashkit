1a90d1347048dc7c498003ff43b3c3f8b20c6075
Markus Goetz <mgmarkusgoetz@gmail.com>
2026-05-29 12:40:07 +0200

    Fix the example simple to use async watch for events

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,4 +1,6 @@
 //! simple devices get and there size
+use flashkit::flash;
+use tokio_stream::StreamExt;
 use tracing::{
     Level,
     info,
@@ -8,18 +10,29 @@ use tracing_subscriber::{
     fmt::format::FmtSpan,
 };
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
+        .with_max_level(Level::INFO)
         .with_span_events(FmtSpan::FULL)
         .finish();
     tracing::subscriber::set_global_default(subscriber)
         .expect("Tracing Subscriber failed to setup");
     info!("Startup");
-    // let flasher = flash();
-    // let devices = flasher.list_devices();
-    // println!("{:#?}", devices);
-    // devices.unwrap().into_iter().for_each(|x| {
-    //     println!("Size: {:?}", x.get_sizes());
-    // });
+    let flasher = flash().await.expect("Flasher failed");
+    let mut device_stream = flasher.watch_devices().await.expect("watch devices failed");
+
+    info!("Watcher active. Awaiting hotplug events...");
+
+    // 2. Loop continuously over the incoming events yielded by the active stream
+    while let Some(event) = device_stream.next().await {
+        match event {
+            flashkit::data_types::DeviceEvent::Added(device) => {
+                info!("Device Attached: {} ({:?})", device.name, device.path);
+            }
+            flashkit::data_types::DeviceEvent::Removed(path) => {
+                info!("Device Detached: {:?}", path);
+            }
+        }
+    }
 }
